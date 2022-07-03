41fe8766b898673e46d97431968a66e2d5ed3915
hehanyang <hehanyang@xunlei.com>
2022-07-03 14:43:38 +0800

    feat: retry

--- consts/consts.go
+++ consts/consts.go
@@ -3,6 +3,7 @@ package consts
 const (
 	ActionCreateTask string = "task:create"
 	ActionDeleteTask string = "task:delete"
+	ActionRetryTask  string = "task:retry"
 	ActionStartup    string = "client:startup"
 	ActionShutdown   string = "client:shutdown"
 	ActionUserAdvise string = "user:advise"
