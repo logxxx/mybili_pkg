2174cd809b10cf3838486b0aac7bdcb0cd951eda
hehanyang <hehanyang@xunlei.com>
2022-07-03 15:33:28 +0800

    feat: home

--- consts/consts.go
+++ consts/consts.go
@@ -1,10 +1,11 @@
 package consts
 
 const (
-	ActionCreateTask string = "task:create"
-	ActionDeleteTask string = "task:delete"
-	ActionRetryTask  string = "task:retry"
-	ActionStartup    string = "client:startup"
-	ActionShutdown   string = "client:shutdown"
-	ActionUserAdvise string = "user:advise"
+	ActionCreateTask   string = "task:create"
+	ActionDeleteTask   string = "task:delete"
+	ActionRetryTask    string = "task:retry"
+	ActionStartup      string = "client:startup"
+	ActionShutdown     string = "client:shutdown"
+	ActionOpenHomePage string = "client:open_homepage"
+	ActionUserAdvise   string = "user:advise"
 )
